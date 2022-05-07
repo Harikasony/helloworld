658a7961488d3ebf083c15a45ee22615212ade35
Harika <harikavuthunuri@gmail.com>
2022-05-07 10:14:54 +0530

    First commit

---
+++ S1hw/src/hello/Esquation.java
@@ -0,0 +1,16 @@
+package hello;
+
+public class Esquation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a= 10;
+float b = 4.5f;
+
+float val = a*a + 2*a*b +b*b;
+
+System.out.println(val);
+
+	}
+
+}
---
+++ S1hw/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HARIKA
+ *
+ */
+module S1hw {
+}
